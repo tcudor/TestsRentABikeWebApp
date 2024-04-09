b74780bd5fbb7d32ed9ad919c0d5a6649826f1eb
tudorg <tudorg@indsoft.ro>
2024-04-09 14:04:08 +0300

    Add project files.

---
+++ BikesControllerTests.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RentABikeWebApp.Controllers;
+using RentABikeWebApp.Data;
+using RentABikeWebApp.Models;
+using RentABikeWebApp.Data.Services;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace RentABikeWebApp.Tests
+{
+    [TestClass]
+    public class BikesControllerTests
+    {
+        private ApplicationDbContext _context;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            _context = new ApplicationDbContext(options);
+            _context.Database.EnsureCreated();
+            SeedDatabase();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+
+        private void SeedDatabase()
+        {
+            var bikes = new[]
+            {
+                new Bike { Id = 1, Type = BikeType.Double, PricePerHour = 20.00m },
+                new Bike { Id = 2, Type = BikeType.Mountain, PricePerHour = 25.00m }
+            };
+            _context.Bikes.AddRange(bikes);
+            _context.SaveChanges();
+        }
+
+
+        [TestMethod]
+        public async Task Edit_ReturnsNotFound_WhenBikeIsNull()
+        {
+            var controller = new BikesController(new BikesService(_context));
+            var result = await controller.Edit(100);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            Assert.AreEqual("NotFound", viewResult.ViewName);
+        }
+
+        [TestMethod]
+        public async Task Edit_ReturnsViewResult_WhenBikeExists()
+        {
+            var controller = new BikesController(new BikesService(_context));
+            var result = await controller.Edit(1);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            Assert.IsNull(viewResult.ViewName);
+        }
+
+        [TestMethod]
+        public async Task Edit_UpdatesBikeInDatabase_WhenModelStateIsValid()
+        {
+            var controller = new BikesController(new BikesService(_context));
+            var existingBike = await _context.Bikes.FindAsync(1);
+            var image = new FormFile(new MemoryStream(new byte[] { }), 0, 0, "imageFile", "image.jpg");
+
+            existingBike.Type = BikeType.Simple;
+            existingBike.PricePerHour = 30.00m;
+
+            var result = await controller.Edit(existingBike.Id, existingBike, image);
+
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            var redirectToActionResult = (RedirectToActionResult)result;
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+
+            var editedBike = await _context.Bikes.FindAsync(existingBike.Id);
+            Assert.AreEqual(existingBike.Type, editedBike.Type);
+            Assert.AreEqual(existingBike.PricePerHour, editedBike.PricePerHour);
+        }
+
+
+
+        [TestMethod]
+        public async Task Delete_ReturnsNotFound_WhenBikeIsNull()
+        {
+            var controller = new BikesController(new BikesService(_context));
+            var result = await controller.Delete(100);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            Assert.AreEqual("NotFound", viewResult.ViewName);
+        }
+
+        [TestMethod]
+        public async Task Delete_ReturnsViewResult_WhenBikeExists()
+        {
+            var controller = new BikesController(new BikesService(_context));
+            var result = await controller.Delete(1);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            Assert.IsNull(viewResult.ViewName);
+        }
+
+        [TestMethod]
+        public async Task DeleteConfirmed_DeletesBikeFromDatabase()
+        { 
+            var controller = new BikesController(new BikesService(_context));
+            await controller.DeleteConfirmed(1);
+            var deletedBike = await _context.Bikes.FindAsync(1);
+            Assert.IsNull(deletedBike);
+        }
+
+        [TestMethod]
+        public async Task Edit_ReturnsNotFound_WhenIdIsInvalid()
+        {
+            var controller = new BikesController(new BikesService(_context));
+            var result = await controller.Edit(-1);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            Assert.AreEqual("NotFound", viewResult.ViewName);
+        }
+
+        [TestMethod]
+        public async Task Delete_ReturnsNotFound_WhenIdIsInvalid()
+        {
+            var controller = new BikesController(new BikesService(_context));
+            var result = await controller.Delete(-1);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            Assert.AreEqual("NotFound", viewResult.ViewName);
+        }
+
+    }
+}
---
+++ BikesServiceTests.cs
@@ -0,0 +1,162 @@
+using Microsoft.EntityFrameworkCore;
+using RentABikeWebApp.Data;
+using RentABikeWebApp.Data.Services;
+using RentABikeWebApp.Models;
+
+namespace TestsRentABikeWebApp
+{
+    [TestClass]
+    public class BikesServiceTests
+    {
+        [TestMethod]
+        public async Task AddAsync_ShouldAddBikeToDatabase()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "AddBikeToDatabase")
+                .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new BikesService(context);
+                var bike = new Bike
+                {
+                    Id = 1,
+                    Type = BikeType.Mountain,
+                    PricePerHour = 15.00m,
+                    Status = "Available",
+                    Image = new byte[] { 0x01, 0x02, 0x03 },
+                };
+
+                await service.AddAsync(bike);
+
+                var result = await context.Bikes.FindAsync(1);
+                Assert.IsNotNull(result);
+                Assert.AreEqual(bike.Id, result.Id);
+                Assert.AreEqual(bike.Type, result.Type);
+                Assert.AreEqual(bike.PricePerHour, result.PricePerHour);
+                Assert.AreEqual(bike.Status, result.Status);
+                CollectionAssert.AreEqual(bike.Image, result.Image);
+            }
+        }
+
+        [TestMethod]
+        public async Task DeleteAsync_ShouldDeleteBikeFromDatabase()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "DeleteBikeFromDatabase")
+                .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var bike = new Bike { Id = 1 };
+                await context.Bikes.AddAsync(bike);
+                await context.SaveChangesAsync();
+
+                var service = new BikesService(context);
+
+                // Act
+                await service.DeleteAsync(1);
+
+                // Assert
+                var result = await context.Bikes.FindAsync(1);
+                Assert.IsNull(result);
+            }
+        }
+
+        [TestMethod]
+        public async Task GetAllAsync_ShouldReturnAllBikesFromDatabase()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "GetAllBikesFromDatabase")
+                .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var bikes = new List<Bike>
+                {
+                    new Bike { Id = 1, Type = BikeType.Simple },
+                    new Bike { Id = 2, Type = BikeType.Mountain },
+                    new Bike { Id = 3, Type = BikeType.Double }
+                };
+                await context.Bikes.AddRangeAsync(bikes);
+                await context.SaveChangesAsync();
+
+                var service = new BikesService(context);
+
+                // Act
+                var result = await service.GetAllAsync();
+
+                // Assert
+                CollectionAssert.AreEqual(bikes, result.ToList());
+            }
+        }
+
+        [TestMethod]
+        public async Task GetByIdAsync_ShouldReturnBikeWithGivenId()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "GetBikeByIdFromDatabase")
+                .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var bikes = new List<Bike>
+                {
+                    new Bike { Id = 1, Type = BikeType.Simple },
+                    new Bike { Id = 2, Type = BikeType.Mountain },
+                    new Bike { Id = 3, Type = BikeType.Double }
+                };
+                await context.Bikes.AddRangeAsync(bikes);
+                await context.SaveChangesAsync();
+
+                var service = new BikesService(context);
+
+                // Act
+                var result = await service.GetByIdAsync(2);
+
+                // Assert
+                Assert.IsNotNull(result);
+                Assert.AreEqual(bikes[1], result);
+            }
+        }
+
+        [TestMethod]
+        public async Task UpdateAsync_ShouldUpdateBikeInDatabase()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "UpdateBikeInDatabase")
+                .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var bike = new Bike { Id = 1, Type = BikeType.Simple, PricePerHour = 15.00m };
+                await context.Bikes.AddAsync(bike);
+                await context.SaveChangesAsync();
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var updatedBike = new Bike { Id = 1, Type = BikeType.Mountain, PricePerHour = 20.00m };
+
+                var service = new BikesService(context);
+
+                // Act
+                await service.UpdateAsync(1, updatedBike);
+
+                // Assert
+                using (var assertContext = new ApplicationDbContext(options))
+                {
+                    var result = await assertContext.Bikes.FindAsync(1);
+                    Assert.IsNotNull(result);
+                    Assert.AreEqual(updatedBike.Type, result.Type);
+                    Assert.AreEqual(updatedBike.PricePerHour, result.PricePerHour);
+                }
+            }
+        }
+
+    }
+}
